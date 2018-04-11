df96766e32ede15a1baa3f5098b8009e9dc445ec
Sweeper777 <sumulang@gmail.com>
2018-04-11 09:25:50 +0100

    implemented Hashable

--- CrossyLines/Node.swift
+++ CrossyLines/Node.swift
@@ -4,4 +4,11 @@ class Node: Hashable {
     var x: CGFloat = 0
     var y: CGFloat = 0
     
+    static func ==(lhs: Node, rhs: Node) -> Bool {
+        return ObjectIdentifier(lhs) == ObjectIdentifier(rhs)
+    }
+    
+    var hashValue: Int {
+        return ObjectIdentifier(self).hashValue
+    }
 }
