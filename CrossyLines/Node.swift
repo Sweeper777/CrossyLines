57ce72da64c39216808397f32f82c7c755b3fd17
Sweeper777 <sumulang@gmail.com>
2018-04-12 09:10:32 +0100

    added a Point struct

--- CrossyLines/Node.swift
+++ CrossyLines/Node.swift
@@ -1,4 +1,21 @@
 import CoreGraphics
+struct Point: Hashable {
+    let x: Int
+    let y: Int
+    
+    init(x: Int, y: Int) {
+        self.x = x
+        self.y = y
+    }
+    
+    static func ==(lhs: Point, rhs: Point) -> Bool {
+        return lhs.x == rhs.x && lhs.y == rhs.y
+    }
+    
+    var hashValue: Int {
+        return x * 31 + y
+    }
+}
 
 class Node: Hashable {
     var x: CGFloat = 0
