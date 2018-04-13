31094785dacb817a50dc15d4eddd4cb65ed32bec
Sweeper777 <sumulang@gmail.com>
2018-04-13 08:56:00 +0100

    added NodeView

---
+++ CrossyLines/NodeView.swift
@@ -0,0 +1,14 @@
+import UIKit
+import SEDraggable
+
+@IBDesignable
+class NodeView: SEDraggable {
+    override func draw(_ rect: CGRect) {
+        let path = UIBezierPath(ovalIn: self.bounds.insetBy(dx: 1.5, dy: 1.5))
+        path.lineWidth = 3
+        UIColor.yellow.setFill()
+        UIColor.black.setStroke()
+        path.fill()
+        path.stroke()
+    }
+}
