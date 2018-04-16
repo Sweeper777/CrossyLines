0e108edb08eb3b1420066408bf17d646594db795
Sweeper777 <sumulang@gmail.com>
2018-04-16 16:11:41 +0200

    added node views when graph is set

--- CrossyLines/GraphView.swift
+++ CrossyLines/GraphView.swift
@@ -6,6 +6,26 @@ class GraphView: UIView {
     private var nodeViewsToNodes: [NodeView: Node] = [:]
     private var displayLink: CADisplayLink!
     
+    var graph: Graph! {
+        didSet {
+            self.subviews.forEach { $0.removeFromSuperview() }
+            nodeViewsToNodes = [:]
+            
+            if graph != nil {
+                for node in graph.nodes {
+                    let nodeView = NodeView(frame: CGRect(x: node.x - 15, y: node.y - 15, width: 30, height: 30))
+                    nodeView.backgroundColor = .clear
+                    nodeView.enableDragging()
+                    nodeView.cagingArea = self.bounds
+                    self.addSubview(nodeView)
+                    nodeViewsToNodes[nodeView] = node
+                }
+            }
+            
+            self.setNeedsDisplay()
+        }
+    }
+    
     override func draw(_ rect: CGRect) {
         if graph == nil { return }
         
