557dcde43890cf5db17cc217059b0d1ee2183a03
Sweeper777 <sumulang@gmail.com>
2018-04-16 16:11:23 +0200

    overrode draw(_:)

--- CrossyLines/GraphView.swift
+++ CrossyLines/GraphView.swift
@@ -6,4 +6,17 @@ class GraphView: UIView {
     private var nodeViewsToNodes: [NodeView: Node] = [:]
     private var displayLink: CADisplayLink!
     
+    override func draw(_ rect: CGRect) {
+        if graph == nil { return }
+        
+        let path = UIBezierPath()
+        path.lineWidth = 2
+        for connection in graph.connections {
+            path.move(to: CGPoint(x: connection.node1.x, y: connection.node1.y))
+            path.addLine(to: CGPoint(x: connection.node2.x, y: connection.node2.y))
+        }
+        UIColor.black.setStroke()
+        path.stroke()
+    }
+    
 }
