12e55f00e2a46c0a164526be504b1ca9dd4c667b
Sweeper777 <sumulang@gmail.com>
2018-04-17 09:06:45 +0100

    overrode supportedInterfaceOrientations

--- CrossyLines/ViewController.swift
+++ CrossyLines/ViewController.swift
@@ -12,5 +12,13 @@ class ViewController: UIViewController {
     }
 
 
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        if self.traitCollection.horizontalSizeClass == .regular &&
+            self.traitCollection.verticalSizeClass == .regular {
+            return .all
+        } else {
+            return .portrait
+        }
+    }
 }
 
