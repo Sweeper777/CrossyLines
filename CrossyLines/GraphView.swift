8544ba6357d0018cb1c6c06b32a7305361197a55
Sweeper777 <sumulang@gmail.com>
2018-04-18 08:49:37 +0100

    color coded connection lines

--- CrossyLines/GraphView.swift
+++ CrossyLines/GraphView.swift
@@ -29,14 +29,20 @@ class GraphView: UIView {
     override func draw(_ rect: CGRect) {
         if graph == nil { return }
         
-        let path = UIBezierPath()
-        path.lineWidth = 2
+        let intersectionDict = graph.checkIntersections()
+        
         for connection in graph.connections {
+            let path = UIBezierPath()
+            path.lineWidth = 2
             path.move(to: CGPoint(x: connection.node1.x, y: connection.node1.y))
             path.addLine(to: CGPoint(x: connection.node2.x, y: connection.node2.y))
+            if intersectionDict[connection] ?? false {
+                UIColor.red.setStroke()
+            } else {
+                UIColor.green.darker().setStroke()
+            }
+            path.stroke()
         }
-        UIColor.black.setStroke()
-        path.stroke()
     }
     
     func syncDrawing() {
