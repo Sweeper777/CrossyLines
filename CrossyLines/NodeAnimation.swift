a767c5a40034c4cc565192a2299566056b0b9f00
Sweeper777 <sumulang@gmail.com>
2018-04-21 08:18:22 +0100

    added NodeAnimation initialiser

--- CrossyLines/NodeAnimation.swift
+++ CrossyLines/NodeAnimation.swift
@@ -14,4 +14,15 @@ class NodeAnimation: CustomStringConvertible {
         return currentFrame >= totalFrames
     }
     
+    init(from: CGPoint, to: CGPoint, totalFrames: Int) {
+        self.from = from
+        self.to = to
+        self.totalFrames = totalFrames
+        
+        currentPosition = from
+        
+        totalLength = sqrt(pow(to.y - from.y, 2) + pow(to.x - from.x, 2))
+        step = totalLength / CGFloat(totalFrames)
+    }
+    
 }
