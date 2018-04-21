a03c674e986b929f05c9cfb70b584e3d8339b95d
Sweeper777 <sumulang@gmail.com>
2018-04-21 08:17:56 +0100

    added NodeAnimation

---
+++ CrossyLines/NodeAnimation.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+class NodeAnimation: CustomStringConvertible {
+    let from: CGPoint
+    let to: CGPoint
+    let totalFrames: Int
+    let step: CGFloat
+    let totalLength: CGFloat
+    
+}
