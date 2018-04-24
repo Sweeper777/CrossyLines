55ebd559651fddf60e0a160a8a8f3d21865f6f17
Sweeper777 <sumulang@gmail.com>
2018-04-24 07:08:22 +0100

    implemented restart button

--- CrossyLines/ViewController.swift
+++ CrossyLines/ViewController.swift
@@ -79,6 +79,10 @@ class ViewController: UIViewController, CircleMenuDelegate {
     
     func circleMenu(_ circleMenu: CircleMenu, buttonDidSelected button: UIButton, atIndex: Int) {
         switch atIndex {
+        case 0:
+            let originalGraph = graph.createCopy()
+            originalGraph.shuffle(within: graphView.bounds)
+            graphView.animate(to: originalGraph, frameCount: 30)
         case 2:
             let insettedRect = self.graphView.bounds.insetBy(dx: 15, dy: 15)
             let graphCopy = solutionGraph.createCopy()
