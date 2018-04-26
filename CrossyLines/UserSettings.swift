f79fd5c1fb8549ba3ef9c7bc4694d0b881053c8a
Sweeper777 <sumulang@gmail.com>
2018-04-26 06:55:55 +0100

    added loadUserSettings

--- CrossyLines/UserSettings.swift
+++ CrossyLines/UserSettings.swift
@@ -4,4 +4,12 @@ class UserSettings {
     static var nodeCount: Int = 7
     static var maxConnectionCount = 4
     static var colorCodeConnections = true
+    
+    static func loadUserSettings() {
+        let defaults = UserDefaults.standard
+        nodeCount = defaults.has(key: "nodeCount") ? defaults.integer(forKey: "nodeCount") : 7
+        maxConnectionCount = defaults.has(key: "maxConnectionCount") ? defaults.integer(forKey: "maxConnectionCount") : 4
+        colorCodeConnections = defaults.has(key: "colorCodeConnections") ? defaults.bool(forKey: "colorCodeConnections") : true
+    }
+    
 }
