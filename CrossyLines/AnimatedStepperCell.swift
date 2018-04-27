96ae2f41bc2462bd9a68a4d25bef499a1b7ccccd
Sweeper777 <sumulang@gmail.com>
2018-04-27 07:41:34 +0100

    added initializers

--- CrossyLines/AnimatedStepperCell.swift
+++ CrossyLines/AnimatedStepperCell.swift
@@ -6,4 +6,12 @@ class AnimatedStepperCell: Cell<Int>, CellType {
     @IBOutlet var stepper: Stepperier!
     @IBOutlet var label: UILabel!
     
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    required init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+    }
+    
 }
