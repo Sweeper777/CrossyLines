31240ebd3fe9cc0e22c1be7a89fffefd64518c2a
Sweeper777 <sumulang@gmail.com>
2018-04-28 08:26:30 +0100

    added nodeCount row

--- CrossyLines/SettingsController.swift
+++ CrossyLines/SettingsController.swift
@@ -6,4 +6,11 @@ class SettingsController: FormViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        form +++ AnimatedStepperRow("nodeCount") {
+            row in
+            row.cell.label.text = "Number of nodes"
+            row.cell.stepper.minimumValue = 7
+            row.cell.stepper.maximumValue = 40
+            row.value = UserSettings.nodeCount
+        }
 }
