b9034de88da0836d44433316a55c42334ddffe7e
Sweeper777 <sumulang@gmail.com>
2018-05-06 08:07:58 +0100

    Revert "tested a stack overflow question"
    This reverts commit 4cfc7fefcab6a6b1222f84efc78537f421242c63.

--- CrossyLines/AppDelegate.swift
+++ CrossyLines/AppDelegate.swift
@@ -8,7 +8,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         UserSettings.loadUserSettings()
-        print(window!.rootViewController!)
         return true
     }
 
