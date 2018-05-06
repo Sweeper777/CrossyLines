4cfc7fefcab6a6b1222f84efc78537f421242c63
Sweeper777 <sumulang@gmail.com>
2018-05-06 08:07:54 +0100

    tested a stack overflow question

--- CrossyLines/AppDelegate.swift
+++ CrossyLines/AppDelegate.swift
@@ -8,6 +8,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         UserSettings.loadUserSettings()
+        print(window!.rootViewController!)
         return true
     }
 
