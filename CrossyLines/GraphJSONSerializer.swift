76868a52448f85894344d0c94c48865f5b74b071
Sweeper777 <sumulang@gmail.com>
2018-05-10 07:19:09 +0100

    serialised nodes to JSON

--- CrossyLines/GraphJSONSerializer.swift
+++ CrossyLines/GraphJSONSerializer.swift
@@ -1,4 +1,12 @@
 import SwiftyJSON
 
 extension Graph {
+    func toJSON() -> JSON {
+        var nodesJSON: JSON = [:]
+        var backwardsDict = [Node: Int]()
+        for (i, node) in self.nodes.enumerated() {
+            nodesJSON["node\(i)"] = ["x": node.x, "y": node.y]
+            backwardsDict[node] = i
+        }
+    }
 }
