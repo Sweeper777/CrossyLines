47b7a3abdf1c7fa862147470d37b663e46d00f8d
Sweeper777 <sumulang@gmail.com>
2018-05-10 07:20:12 +0100

    deserialised nodes

--- CrossyLines/GraphJSONSerializer.swift
+++ CrossyLines/GraphJSONSerializer.swift
@@ -22,6 +22,14 @@ extension Graph {
         var connections = Set<Connection>()
         
         var dict: [Int: Node] = [:]
+        for (key, jsonNode) in jsonNodes {
+            guard let x = jsonNode["x"].double else { return nil }
+            guard let y = jsonNode["y"].double else { return nil }
+            let node = Node(x: x.f, y: y.f)
+            nodes.append(node)
+            guard let nodeNumber = Int(String(key.dropFirst(4))) else { return nil }
+            dict[nodeNumber] = node
+        }
         return Graph(nodes: nodes, connections: connections)
     }
 }
