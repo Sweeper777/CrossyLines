9f7a53425b4827c0cfc7a332203e1f37d97c46c6
Sweeper777 <sumulang@gmail.com>
2018-05-17 08:09:11 +0100

    tested whether applicationWillTerminate is called when stop debugging

--- CrossyLines/AppDelegate.swift
+++ CrossyLines/AppDelegate.swift
@@ -30,7 +30,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+        print("wilTerminate")
     }
 
 
